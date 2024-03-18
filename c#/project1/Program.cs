3f7429ab26d22c71427e2a12af162f3635d6cbfb
Yerkebulan <erkebulanlbk@gmail.com>
2024-03-18 12:41:50 +0500

    changes

---
+++ c#/project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project1 {
+    class Program{
+        static void Main(){
+            /*  output of program
+              ;jsrgn;skm*/
+            Console.Write("ahahahah1");
+            Console.Write("ahahahah2");
+            Console.Write("ahahahah3");
+            Console.Write("ahahahah4\n");
+            Console.ReadLine();
+        }
+    }
+}
+
