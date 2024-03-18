683a72438fb7cbb308650e430be90400c11dd2af
Yerkebulan <erkebulanlbk@gmail.com>
2024-03-18 14:21:42 +0500

    c#

--- c#/project1/obj/Debug/net8.0/WEB.AssemblyInfo.cs
+++ c#/project1/obj/Debug/net8.0/WEB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c262fa2712f26a8c8ecc1d4e4a253ffbf7726b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa12daa34f1b02de460d9a3129a9444bdf4ad91")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
