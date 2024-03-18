46769bcc3b9213d89f4c332ce480055de1ee2e4b
Yerkebulan <erkebulanlbk@gmail.com>
2024-03-18 12:41:56 +0500

    changes

--- c#/project1/Program.cs
+++ c#/project1/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 
-namespace project1 {
+namespace project1{
     class Program{
         static void Main(){
-            /*  output of program
-              ;jsrgn;skm*/
-            Console.Write("ahahahah1");
-            Console.Write("ahahahah2");
-            Console.Write("ahahahah3");
-            Console.Write("ahahahah4\n");
-            Console.ReadLine();
+            // float
+            // short
+            //byte
+            // int
+            // long
+            // double
+
+            // Console.ReadKey()
+
         }
     }
 }
-
